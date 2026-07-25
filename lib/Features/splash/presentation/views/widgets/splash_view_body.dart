a3304d41f76e1d6aa05d7a99d0c5fa7a8b1a9153
Mmedhat10 <mohmedmedhat220@gmail.com>
2026-07-25 14:41:49 +0300

    feat: Refactor SplashViewBody to use StatefulWidget with sliding animation

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,23 +1,48 @@
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsData.logo),
+        SlideTransition(
+          position: slidingAnimation,
+          child: Image.asset(AssetsData.logo)),
         const SizedBox(
           height: 4,
         ),
-        Text(
-          "Read Free Books",
-          style: TextStyle(fontSize: 20),
-          textAlign: TextAlign.center,
+        SlideTransition(
+          position: slidingAnimation,
+          child: Text(
+            "Read Free Books",
+            style: TextStyle(fontSize: 20),
+            textAlign: TextAlign.center,
+          ),
         ),
       ],
     );
